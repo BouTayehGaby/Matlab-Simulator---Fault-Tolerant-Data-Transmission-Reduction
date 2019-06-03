b5c20723ff968931c2fd9c1dcec84d6e78b670d9
Gaby <gaby.boutayeh@gmail.com>
2019-06-03 11:21:01 +0200

    2nd Commit

---
+++ HLMS_Miss.m
@@ -0,0 +1,219 @@
+function HLMSV3_Miss(sensor,feature,maxerr,mu1)
+    %clc
+    close all
+    Nsamples=100000;
+    fprintf('%d-----------------------------\n',sensor)
+    %############################# Settings #########################################
+     feature=4;                          % 3 = Temperature , 4= Humidity , 6=Infrared
+     maxerr=0.1;                         % Maximum error treshold
+    %#################################################################################
+    arraysent=zeros(1,1);
+    arrayperc=zeros(1,1);
+    arrayMiss=zeros(1,1);
+    arrayerror=zeros(1,10);
+	
+for GG=1:1   
+    for G=sensor:sensor
+        fprintf('%d-----------------------------\n',G)
+        NumberOfUpdate=0;                              % Number of Samples
+        mote=num2str(G);
+        filename = strcat('sensor_',mote,'.txt');                        
+
+        x = dlmread(filename,' ',[0 feature Nsamples-1 feature]);
+
+        %############################# Variables ##########################################
+        m1=3;                               % Lenght of each filter 
+        m2=2;                               % Number of subfilters
+        sent=0;
+        flag=0;
+        euc=0;
+        errc=0;
+        Miss=0;
+        attemptSend=0;
+        %#################################### Files ########################################
+        fresults = fopen('fresults.txt','w');
+        Transmitted = fopen('Transmitted.txt','w');
+        errorDiff = fopen('errorDiff.txt','w');
+        index = 'index.txt';
+        indexes = dlmread(index,' ');
+        indexes((numel(indexes)+1):100000)=0;
+        %###################################################################################
+
+        %Initializing coeficient Matrix W1(n) , wd=[w11,w12,w13;w21,w22,w23];
+        wd=[0,0,0;0,0,0];
+
+        %Single filter at Level 2 joing the two filters at an upper level. 
+        %wd2=[w1,w2];
+        wd2=[0,0];
+
+        %output of both level 1 filters, yd=[y11,y12];
+        yd=[0,0]; % output of the two upper layer filters
+
+        %error outputetd by both level 1 filters, ed=[e11,e12];
+        ed=[0,0];
+
+        ud=[x(6),x(5),x(4),x(3),x(2),x(1)];
+        sent=sent+6;
+        fprintf(fresults,'%d\n%d\n%d\n%d\n%d\n%d\n',x(1),x(2),x(3),x(4),x(5),x(6));
+        fprintf(Transmitted,'%d\n%d\n%d\n%d\n%d\n%d\n',x(1),x(2),x(3),x(4),x(5),x(6));
+        fprintf(errorDiff,'%d\n%d\n%d\n%d\n%d\n%d\n',0,0,0,0,0,0);
+
+        n=7;
+        k=1;
+        desired=ud(6);
+		
+		p1=(1/3*(power(ud(3),2)))+(1/3*(power(ud(2),2)))+(1/3*(power(ud(1),2)));
+		p2=(1/3*(power(ud(4),2)))+(1/3*(power(ud(5),2)))+(1/3*(power(ud(6),2)));
+		if p1>p2
+		  mu1=0+rand(1,1)*((2/p1)-0);
+		else
+		  mu1=0+rand(1,1)*((2/p2)-0);
+		end
+		
+        while n < Nsamples+1 
+
+            %ligne 3,4,5 in the Algo
+            for i=1:m2
+                yd(i)=0;
+                for j=1:m1
+                    yd(i)= yd(i)+(wd(i,j)*ud(7-(i-1)*m1-j));
+                end
+            end 
+
+            %ligne 6,7,8 in Algo
+            for i=1:m2
+                ed(i)=desired-yd(i);
+            end
+
+            %ligne 9->13 in Algo
+            for i=1:m2
+                for j=1:m1
+                wd(i,j)=wd(i,j)+(mu1*ed(i)*ud(7-(i-1)*m1-j));
+                end
+            end
+
+            y=0;
+            for i=1:m2
+                y=y+(wd2(i)*yd(i));
+            end
+
+            error=desired-y;
+            fprintf(errorDiff,'%d\n',error);
+			
+			if  yd(1)~=0 && yd(2)~=0
+				p3=(1/2*(power(yd(1),2)))+(1/2*(power(yd(2),2)));
+				mu2=0+rand(1,1)*((2/p3)-0);
+			end
+
+            %ligne 18 to ligne 20
+            for i=1:m2
+                wd2(i)=wd2(i)+(mu2*error*yd(i));
+            end
+
+            if indexes(k) ~= n   %if in the index file at n there is a transmission
+
+                euc=euc+power(abs(x(n)-y),2);
+                errc = errc+1;
+
+                %Update the filter on the sensor
+
+                fprintf(fresults,'%d\n',y);
+                fprintf(Transmitted,'%s\n','nan');
+                temp=zeros(1,6);
+                temp(2:6)=ud(1:5); % shift array to the left and add the newest input to the far right box
+                temp(1)=y; 
+                ud=temp;
+                desired=ud(1);
+            else
+                k=k+1;
+                % count how many attempt to send a reading has been tried.
+                attemptSend=attemptSend+1;
+
+                %if we are not in a missing phase
+                if flag==0
+                    %Miss or no ?
+                    R = binornd(1,0.5); %0.1=10%
+                    % how many succesive misses
+                    c = randi([0 0]);
+                else
+                    r=0;
+                    c=c-1;
+                end
+                if R=1 %Miss
+                    Miss=Miss+1;
+                    euc=euc+power(abs(x(n)-y),2);
+                    errc = errc+1;
+
+                    %Missed transmission update Sink filter according to the
+                    %prediction outputed.
+% 
+                    fprintf(fresults,'%d\n',y);
+                    fprintf(Transmitted,'%s\n','nan');
+                    temp=zeros(1,6);
+                    temp(2:6)=ud(1:5); % shift array to the left and add the newest input to the far right box
+                    temp(1)=y; 
+                    ud=temp;
+                    desired=ud(1);
+                    flag=1;
+                    if c==0
+                        flag=0;
+                    end
+                else
+
+                    %succesfull transmission update the Sinks filter using the
+                    %real recieved value.
+                    NumberOfUpdate=NumberOfUpdate+1;
+                    sent=sent+1;
+                    fprintf(fresults,'%d\n',x(n));
+                    fprintf(Transmitted,'%d\n',x(n));
+                    temp=zeros(1,6);
+                    temp(2:6)=ud(1:5); % shift array to the left and add the newest input to the far right box
+                    temp(1)=x(n); 
+                    ud=temp;
+                    desired=x(n);
+                end
+            end
+            n=n+1;
+        end
+
+         fprintf('number of sent measurements: %d\n',sent);
+         fprintf('percentage of trasmitted data:  %f\n',(100*sent)/Nsamples);
+         fprintf('Average error: %f\n',(euc/errc));
+         fprintf('Number of adaptation: %f\n',NumberOfUpdate);
+         fprintf('%d\n',sent);
+         fprintf('%f\n',(100*sent)/Nsamples);
+         fprintf('%d\n',Miss);
+         fprintf('%f\n',sqrt(euc/errc));
+        
+        arraysent(GG)=sent;
+        arrayperc(GG)=(100*sent)/Nsamples;
+        arrayMiss(GG)=Miss;
+        arrayerror(GG)=sqrt(euc/errc);
+
+
+
+    end 
+
+end
+
+    results='fresults.txt';
+    x1=dlmread(results,' ',[0 0 Nsamples-1 0]);
+
+    rr=0;
+    for i=1:Nsamples
+        if abs(x(i)-x1(i))>maxerr
+            rr=rr+1;
+            euc=euc+power(abs(x(i)-x1(i)),2);
+            errc = errc+1;
+        end
+    end
+    fprintf('%d-----------------------------\n',G)
+    fprintf('%f\n',mean(arraysent));
+    fprintf('%f\n',mean(arrayperc));
+    fprintf('%f\n',mean(arrayMiss));
+    fprintf('%f\n',sqrt(euc/errc));
+    fprintf('%f\n',rr);
+    fprintf('--\n');
+    fprintf('--\n');
+    fclose('all');
+end
